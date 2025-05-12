cdd0d070e5e5532569f72c26f6fa48430a5797b1
Chelsea Afoakwah <chelseaafoa1@gmail.com>
2025-05-11 22:07:09 -0400

    testing new changes on vercel

--- api/index.js
+++ api/index.js
@@ -3,23 +3,12 @@ const serverless = require('serverless-http');
 
 const app = express();
 
-// Favicon fix
-app.get('/favicon.ico', (req, res) => res.status(204).end());
-
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/about', (req, res) => {
-  res.send('About route');
-});
-
 app.get('/quote', (req, res) => {
   res.json({ quote: "Testing", author: "chelseaafoa1" });
-});
-
-app.use((req, res) => {
-  res.status(404).send('404 Not Found');
 });
 
 module.exports = serverless(app);
