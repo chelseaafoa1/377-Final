2c3d8198691e7ebd1105ba102fa491c81648df2d
Chelsea Afoakwah <chelseaafoa1@gmail.com>
2025-05-11 22:16:45 -0400

    more changes

--- api/index.js
+++ api/index.js
@@ -8,7 +8,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/quote', (req, res) => {
-  res.json({ quote: "Testing", author: "chelseaafoa1" });
+  res.json({ quote: "You got this, Chelsea!", author: "ChatGPT" });
+});
+
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
+app.use((req, res) => {
+  res.status(404).send('404 Not Found');
 });
 
 module.exports = serverless(app);
