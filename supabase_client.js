38131308eb7ddbd68acca173ed344e9bc46ccb2f
chelseaafoa1 <166977199+chelseaafoa1@users.noreply.github.com>
2025-05-16 22:24:37 -0400

    Add files via upload

---
+++ supabase_client.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase_url = 'https://nvpoqnnkmdsrmscxyzmo.supabase.co';
+const supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cG9xbm5rbWRzcm1zY3h5em1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczNDQ4MjUsImV4cCI6MjA2MjkyMDgyNX0.68OU3jaD6mNr_gRCb5hKabzAee7lvgDD-68vqE7pR80';
+export const supabase = createClient(supabase_url, supabase_key);
