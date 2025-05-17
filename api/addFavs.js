ec41ddfe04441941a6b7be0431961afa4bfbcf95
Chelsea Afoakwah <chelseaafoa1@gmail.com>
2025-05-16 23:03:14 -0400

    fix vercel routing and quotes

---
+++ api/addFavs.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res){
+    if (req.method !=='POST') {
+        return res.status(405).json({error: 'only POST request'})
+    }
+
+    const {createClient} = require('@supabase/supabase-js');
+   const supabase=createClient(
+        process.env.SUPABASE_URL,
+        process.env.SUPABASE_ANON_KEY
+);
+
+const {quote, author}=req.body;
+
+const { data, error } = await supabase
+    .from('favorites')
+    .insert([{quote, author}]);
+
+if (error) {
+    return res.status(500).json({ error: error.message})
+}
+res.status(200).json({message: 'Saved!', data});
+}
+
